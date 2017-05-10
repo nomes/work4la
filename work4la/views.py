062e1c08db707fe74a5b174b43dc455e064bc655
Alex Chao <alex.h.chao@gmail.com>
2017-05-09 20:57:24 -0700

    Add skeleton home template

--- work4la/views.py
+++ work4la/views.py
@@ -1,6 +1,6 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
+@view_config(route_name='home', renderer='templates/home.jinja2')
 def my_view(request):
-    return {'project': 'work4la'}
+    return {}
